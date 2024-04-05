f5b364ab978d8bfc508596d907a27cd38aa23cae
Heize <dev.heize@gmail.com>
2024-04-05 13:05:44 +0900

    feat: blog-archive

--- bookmark/views.py
+++ bookmark/views.py
@@ -11,5 +11,8 @@ class BookmarkLV(ListView):
     # template_name = 'bookmark/bookmark_list.html' #[app_name]/[model_name]_list.html
     # context_object_name = 'object_list' #object_list
 
+# DetailView
 class BookmarkDV(DetailView):
     model=Bookmark
+    # template_name = 'bookmark/bookmark_detail.html' #[app_name]/[model_name]_detail.html
+        # context_object_name = 'object' #object
--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('bookmark/', include('bookmark.urls')),
     path('admin/', admin.site.urls),
+    path("blog/", include("blog.urls")),
 ]
